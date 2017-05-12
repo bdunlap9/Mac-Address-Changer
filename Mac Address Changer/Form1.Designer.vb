9dd7bc2c0d5b1f3b77483215b65cab7a1423f611
Weeke <baileydunlap9@gmail.com>
2017-05-12 13:31:30 -0700

    Add files via upload

---
+++ Mac Address Changer/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.combo_network = New System.Windows.Forms.ComboBox()
+        Me.label_mactext = New System.Windows.Forms.Label()
+        Me.mac_text = New System.Windows.Forms.TextBox()
+        Me.bt_update = New System.Windows.Forms.Button()
+        Me.bt_defaultmac = New System.Windows.Forms.Button()
+        Me.MacInputLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 220)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(120, 13)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Invictus Profile - Weeke"
+        '
+        'combo_network
+        '
+        Me.combo_network.FormattingEnabled = True
+        Me.combo_network.Location = New System.Drawing.Point(11, 12)
+        Me.combo_network.Name = "combo_network"
+        Me.combo_network.Size = New System.Drawing.Size(390, 21)
+        Me.combo_network.TabIndex = 2
+        '
+        'label_mactext
+        '
+        Me.label_mactext.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label_mactext.Location = New System.Drawing.Point(11, 139)
+        Me.label_mactext.Name = "label_mactext"
+        Me.label_mactext.Size = New System.Drawing.Size(389, 81)
+        Me.label_mactext.TabIndex = 3
+        '
+        'mac_text
+        '
+        Me.mac_text.Location = New System.Drawing.Point(144, 39)
+        Me.mac_text.Name = "mac_text"
+        Me.mac_text.Size = New System.Drawing.Size(257, 20)
+        Me.mac_text.TabIndex = 4
+        '
+        'bt_update
+        '
+        Me.bt_update.Location = New System.Drawing.Point(11, 94)
+        Me.bt_update.Name = "bt_update"
+        Me.bt_update.Size = New System.Drawing.Size(127, 23)
+        Me.bt_update.TabIndex = 5
+        Me.bt_update.Text = "Get New Mac Address"
+        Me.bt_update.UseVisualStyleBackColor = True
+        '
+        'bt_defaultmac
+        '
+        Me.bt_defaultmac.Location = New System.Drawing.Point(11, 65)
+        Me.bt_defaultmac.Name = "bt_defaultmac"
+        Me.bt_defaultmac.Size = New System.Drawing.Size(127, 23)
+        Me.bt_defaultmac.TabIndex = 6
+        Me.bt_defaultmac.Text = "Reset Mac Address"
+        Me.bt_defaultmac.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.bt_defaultmac.UseVisualStyleBackColor = True
+        '
+        'MacInputLabel
+        '
+        Me.MacInputLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MacInputLabel.Location = New System.Drawing.Point(8, 39)
+        Me.MacInputLabel.Name = "MacInputLabel"
+        Me.MacInputLabel.Size = New System.Drawing.Size(130, 23)
+        Me.MacInputLabel.TabIndex = 7
+        Me.MacInputLabel.Text = "New Mac Address:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(158, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "This is your default mac address"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(407, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(256, 50)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "To get a mac address to change to use this site: http://www.miniwebtool.com/mac-a" &
+    "ddress-generator"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(407, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(208, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Must be formatted like: B9:99:75:CA:F4:C5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(675, 243)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MacInputLabel)
+        Me.Controls.Add(Me.bt_defaultmac)
+        Me.Controls.Add(Me.bt_update)
+        Me.Controls.Add(Me.mac_text)
+        Me.Controls.Add(Me.label_mactext)
+        Me.Controls.Add(Me.combo_network)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Name = "Form1"
+        Me.Text = "Mac Address Changer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents combo_network As ComboBox
+    Friend WithEvents label_mactext As Label
+    Friend WithEvents mac_text As TextBox
+    Friend WithEvents bt_update As Button
+    Friend WithEvents bt_defaultmac As Button
+    Friend WithEvents MacInputLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Mac Address Changer/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports System.Management
+
+Public Class Form1
+    ' Coded by Weeke
+
+    ' I used the source code from http://forum.codecall.net/topic/43009-see-and-change-your-mac-address-in-vbnet/
+    ' I really only had to put it together and fix a couple lines of code and then compile and add labels telling you how to use this tool as well as link a
+    ' Mac Address Generator so it would be user friendly for everyone.
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("https://forum.invictus-load.in/profile/3056-weeke/")
+    End Sub
+
+    Private Sub FillNetworkAdapters()
+
+        Dim mc As System.Management.ManagementClass
+        Dim mo As ManagementObject
+
+        mc = New ManagementClass("Win32_NetworkAdapterConfiguration")
+
+        Dim moc As ManagementObjectCollection = mc.GetInstances()
+
+        For Each mo In moc
+            If mo.Item("IPEnabled") = True Then
+                Dim strAdapter As String
+                strAdapter = mo.Item("Caption").ToString().Substring(11)
+
+                combo_network.Items.Add(strAdapter)
+            End If
+        Next
+
+    End Sub
+
+    Private Sub MainForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FillNetworkAdapters()
+
+    End Sub
+
+    Private Function GetMACAddress(ByVal Adapter As String) As String
+
+        Dim mc As System.Management.ManagementClass
+        Dim mo As ManagementObject
+
+        mc = New ManagementClass("Win32_NetworkAdapterConfiguration")
+
+        Dim moc As ManagementObjectCollection = mc.GetInstances()
+
+        For Each mo In moc
+            If mo.Item("IPEnabled") = True Then
+                Dim strAdapter As String
+                strAdapter = mo.Item("Caption").ToString().Substring(11)
+                If strAdapter = Adapter Then
+                    Return mo.Item("MacAddress").ToString()
+                End If
+            End If
+        Next
+
+    End Function
+
+    Private Sub combo_network_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles combo_network.SelectedIndexChanged
+
+        label_mactext.Text = GetMACAddress(combo_network.SelectedItem.ToString)
+
+    End Sub
+
+    Private Function DoPadding(ByVal x As String) As String
+        Dim Ret As String
+        Dim z As Integer
+
+        Ret = x
+
+        If Len(x) < 4 Then
+            For z = 1 To 4 - Len(x)
+                Ret = "0" & Ret
+            Next
+        End If
+
+        Return Ret
+
+    End Function
+
+    Private Sub ShowRestart()
+
+        Dim res As MsgBoxResult = MsgBox("Your MAC Address has been changed. In order to make the changes take effect, either restart your computer or enable and disable the changed Network Adapter.", MsgBoxStyle.Information)
+
+    End Sub
+
+    Private Sub bt_update_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_update.Click
+        If IsOkay() = False Then
+            Exit Sub
+        End If
+
+        Dim regKey As Microsoft.Win32.RegistryKey
+        Dim Addr As String = GetRoot(combo_network.SelectedItem.ToString())
+
+        regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(Addr, True)
+        regKey.SetValue("NetworkAddress", mac_text.Text.Replace(":", ""))
+
+        ShowRestart()
+    End Sub
+
+    Private Function GetRoot(ByVal Adapter As String) As String
+        Dim regKey As Microsoft.Win32.RegistryKey
+        Dim i As Integer = 0
+
+        Do
+            Dim Root As String = "SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\"
+            Dim Last As String = DoPadding(i)
+            regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(Root & Last, True)
+
+            Try
+                Dim cAdapter As String = regKey.GetValue("DriverDesc").ToString()
+                If cAdapter = Adapter Then
+                    Return Root & Last
+                End If
+            Catch
+                Exit Do
+            End Try
+
+            i += 1
+        Loop
+
+    End Function
+
+    Private Function IsOkay() As Boolean
+
+        If mac_text.Text = "" Then
+            MsgBox("You didn't enter a MAC Address", MsgBoxStyle.Critical)
+            Return False
+        End If
+
+        Dim ed As String = mac_text.Text.Replace(":", "")
+
+        If ed.Length <> 12 Then
+            MsgBox("A MAC Address must have a length of 12", MsgBoxStyle.Critical)
+            Return False
+        End If
+
+        Try
+            If combo_network.SelectedItem.ToString = "" Then
+                MsgBox("No Network Adapter selected", MsgBoxStyle.Critical)
+                Return False
+            End If
+        Catch
+            MsgBox("No Network Adapter selected", MsgBoxStyle.Critical)
+            Return False
+        End Try
+
+
+        Dim noerror As Boolean = True
+        Dim i As Integer
+
+        For i = 0 To ed.Length - 1
+            If IsHex(ed.Substring(i, 1)) = False Then
+                MsgBox("MAC Address in wrong format", MsgBoxStyle.Critical)
+                Return False
+            End If
+        Next
+
+        Return True
+
+    End Function
+
+    Private Function IsHex(ByVal l As String) As Boolean
+        Dim table As String = "0123456789ABCDEF"
+        Dim i As Integer
+
+        For i = 0 To table.Length - 1
+            If l = table.Substring(i, 1) Then
+                Return True
+            End If
+        Next
+        Return False
+
+    End Function
+
+    Private Sub bt_defaultmac_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_defaultmac.Click
+        If IsOkay() = False Then
+            Exit Sub
+        End If
+
+        Dim regKey As Microsoft.Win32.RegistryKey
+        Dim Addr As String = GetRoot(combo_network.SelectedItem.ToString())
+
+        regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(Addr, True)
+
+        Try
+            regKey.DeleteValue("NetworkAddress")
+        Catch
+            'Do NOTHING
+        End Try
+
+        ShowRestart()
+
+    End Sub
+
+End Class
